c866236cca8b71330bfdb99b51654bd3f9e02a49
96Manis <1996manishrai@gmail.com>
2023-12-22 20:31:47 +0530

    todo app

---
+++ index.js
@@ -0,0 +1,25 @@
+let myform=document.querySelector("form")
+let inptask=document.getElementById("task")
+let priority=document.getElementById("priority")
+let alldata=[];
+let tbody=document.querySelector("tbody")
+myform.addEventListener("submit",function(e){
+    e.preventDefault();
+    let data={};
+    data.input1=inptask.value
+    data.input2=priority.value
+    alldata.push(data)
+    // console.log(data)
+    tbody.innerHTML=null
+
+    alldata.map((ele)=>
+    { let row= document.createElement("tr")
+      let td1=document.createElement("td")
+      let td2=document.createElement("td")
+      td1.innerText=ele.input1
+      td2.innerText=ele.input2
+      row.append(td1,td2)
+      tbody.append(row)
+
+    })
+})
